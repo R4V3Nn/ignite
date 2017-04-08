3279c2750d0e6bd209f731c14c15499f1033b2c3
Artem <artem.kaseka@gmail.com>
2017-04-08 16:23:33 +0300

    Add slider

--- JSproject/js/main.js
+++ JSproject/js/main.js
@@ -4,8 +4,8 @@
 
   toggle.className = 'is-visible';
   nav.className = 'is-hidden';
-  
-  toggle.addEventListener('click', function() {
+
+  toggle.addEventListener('click', function () {
     if (nav.className === 'is-hidden') {
       nav.className = '';
     } else {
@@ -13,3 +13,52 @@
     }
   })
 })();
+
+//SLIDER
+
+var slideIndex = 0;
+
+var next = document.getElementsByClassName("next")[0].onclick = function plusSlides() {
+  showSlides(slideIndex += 1);
+
+  clearInterval(auto);
+  setTimeout(function () {
+    auto = window.setInterval(autoPlay, 3000);
+  }, 5000);
+};
+
+
+var prev = document.getElementsByClassName("prev")[0].onclick = function minusSlides() {
+  showSlides(slideIndex -= 1);
+
+  clearInterval(auto);
+  setTimeout(function () {
+    auto = window.setInterval(autoPlay, 3000);
+  }, 5000);
+};
+
+
+function showSlides() {
+  var slides = document.getElementsByClassName("slide");
+
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+}
+
+function autoPlay() {
+  slideIndex++;
+  showSlides();
+}
+
+var auto = window.setInterval(autoPlay, 3000);
